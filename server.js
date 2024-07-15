84e415e476b77686285dbb39d9616aeaba598d9b
Ayush Pandey <ayush.pandey6578@gmail.com>
2024-07-15 10:18:36 +0530

    Cors Configuration

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express =require('express');
 const app = express();
+const cors = require('cors');
 require("dotenv").config();
 const dbConfig = require('./config/dbConfig.js');
 const portfolioRoute = require('./routes/portfolioRoutes.js');
 app.use(express.json());
 
+app.use(cors())
 
 app.use('/api/portfolio',portfolioRoute);
 
